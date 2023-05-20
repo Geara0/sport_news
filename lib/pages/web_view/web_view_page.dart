fdfef12a9644b568ec5cb3d94dd8fe51a815c35d
Geara0 <adrezz.26.5@gmiail.com>
2023-05-20 10:32:48 +0500

    change webview dependency

--- lib/pages/web_view/web_view_page.dart
+++ lib/pages/web_view/web_view_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:sport_news/pages/web_view/web_view_interface.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class WebViewPage extends StatefulWidget {
-  const WebViewPage({required this.uri, super.key});
+  const WebViewPage({
+    required this.uri,
+    super.key,
+  });
 
   final Uri uri;
 
@@ -12,20 +14,45 @@ class WebViewPage extends StatefulWidget {
 }
 
 class _WebViewPageState extends State<WebViewPage> {
-  late final WebViewController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController()..loadRequest(widget.uri);
-  }
+  late final InAppWebViewController _controller;
+  var loadingPercentage = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
+    return WillPopScope(
+      onWillPop: () async {
+        // without await to load page on the go
+        _controller.goBack();
+        return false;
+      },
+      child: Padding(
         padding: EdgeInsets.only(top: MediaQuery.paddingOf(context).top),
-        child: WebViewInterface(controller: controller),
+        child: Stack(
+          children: [
+            InAppWebView(
+              initialUrlRequest: URLRequest(url: widget.uri),
+              initialOptions: InAppWebViewGroupOptions(
+                crossPlatform: InAppWebViewOptions(
+                  useShouldOverrideUrlLoading: true,
+                  javaScriptCanOpenWindowsAutomatically: true,
+                  javaScriptEnabled: true,
+                ),
+              ),
+              onWebViewCreated: (controller) => _controller = controller,
+              androidOnPermissionRequest:
+                  (controller, origin, resources) async {
+                return PermissionRequestResponse(
+                  resources: resources,
+                  action: PermissionRequestResponseAction.GRANT,
+                );
+              },
+              onProgressChanged: (controller, progress) =>
+                  setState(() => loadingPercentage = progress),
+            ),
+            if (loadingPercentage < 100)
+              LinearProgressIndicator(value: loadingPercentage / 100.0),
+          ],
+        ),
       ),
     );
   }
